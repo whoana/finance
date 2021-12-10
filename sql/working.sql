75071ae4df568b8a3ac7875e663cfddd6a99dd2b
whoana <whoana@MacBookPro2013Late.local>
2021-12-10 13:59:18 +0900

    good job

--- sql/working.sql
+++ sql/working.sql
@@ -39,6 +39,75 @@ and a.TDATE = b.TRADE_DATE;
                      )  as cprice
                  from quotes a
                  where a.tdate <= '20181231'
-             ) a;
+             ) a
+            ;
+
+select (141 * 23 + 136 * 11)/34.0 from dual;
+        select
+              a.symbol
+             ,'20181231'      as tdate
+             ,avg(a.pprice)   as price
+             ,sum(a.quantity) as qty
+             ,(
+             select b.CLOSE
+             from TYAH005 b
+             where b.TRADE_DATE =
+                   (select min(TRADE_DATE) from TYAH005 where SYMBOL = a.SYMBOL and TRADE_DATE >= '20181231')
+               and b.SYMBOL = a.SYMBOL
+             )  as cprice
+        from quotes a
+        where a.tdate <= '20181231'
+     group by a.symbol
+        union all
+        select
+              a.symbol
+             ,'20190101'      as tdate
+             ,avg(a.pprice)   as price
+             ,sum(a.quantity) as qty
+             ,(
+            select b.CLOSE
+            from TYAH005 b
+            where b.TRADE_DATE =
+                  (select min(TRADE_DATE) from TYAH005 where SYMBOL = a.SYMBOL and TRADE_DATE >= '20190101')
+              and b.SYMBOL = a.SYMBOL
+            )  as cprice
+        from quotes a
+        where a.tdate <= '20190101'
+        group by a.symbol
+        union all
+        select
+            a.symbol
+             ,'20190102'      as tdate
+             ,avg(a.pprice)   as price
+             ,sum(a.quantity) as qty
+             ,(
+            select b.CLOSE
+            from TYAH005 b
+            where b.TRADE_DATE =
+                  (select min(TRADE_DATE) from TYAH005 where SYMBOL = a.SYMBOL and TRADE_DATE >= '20190102')
+              and b.SYMBOL = a.SYMBOL
+            )  as cprice
+        from quotes a
+        where a.tdate <= '20190102'
+        group by a.symbol
+;
 
 
+        select * from TYAH005 where SYMBOL = 'VTI' and TRADE_DATE >= '20181231' ;
+
+/* 포트폴리오-수익률(일) */
+CREATE TABLE TSUM001 (
+                         PORTFOLIO_ID VARCHAR2(50) NOT NULL, /* 포트폴리오ID */
+                         SYMBOL VARCHAR2(10) NOT NULL, /* 심볼 */
+                         DAY VARCHAR2(8) NOT NULL, /* 거래일 */
+                         QTY INTEGER, /* 매입수량 */
+                         PRICE DECIMAL(13,5), /* 평균매입단가 */
+                         AMT NUMBER(13), /* 매입금액 */
+                         CUR_PRICE DECIMAL(13,5), /* 현재가 */
+                         CUR_AMT NUMBER(13), /* 평가금액 */
+                         EARNING NUMBER(13), /* 수익 */
+                         EARNING_RATE DECIMAL(13,5), /* 수익률 */
+                         DAILY_EARNING NUMBER(13), /* 일수익 */
+                         DAILY_EARNING_RATE DECIMAL(13,5), /* 일수익률 */
+                         REG_DATE VARCHAR2(17) /* 등록일 */
+);
